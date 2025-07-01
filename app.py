de49134747c5479343427d378daee42495c4b62b
0bx0 <debrajghosh232@gmail.com>
2025-07-01 15:10:42 +0000

    Added most of the frontend, still needs some ui tweaking

--- app.py
+++ app.py
@@ -6,5 +6,14 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/config_page')
+def config_page():
+    return render_template("config.html")
+
+# create a new route for the timer page saved as timer_page.html
+@app.route('/timer_page')
+def timer_page():
+    return render_template("timer_page.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
